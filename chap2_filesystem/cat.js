f77d6427a1e905ab3393de0b6bb9d6a05ac0ce40
Matthew Muller <mullermw@gmail.com>
2015-06-05 15:55:35 -0700

    Adding comment to cat.js

--- chap2_filesystem/cat.js
+++ chap2_filesystem/cat.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node --harmony
 
+   //this program simply writes the contents of the argument file to stdout.
+
     require('fs').createReadStream(process.argv[2]).pipe(process.stdout);
