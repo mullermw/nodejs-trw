3ae64c89616e7808e0fdd23272903131debeefa4
Matthew Muller <mullermw@gmail.com>
2015-06-05 17:50:13 -0700

    Adding chap3_networking

---
+++ chap3_networking/net-watcher-json-test-service.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node --harmony 
+
+"use strict";
+const
+
+    net = require('net'),
+    server = net.createServer(function(connection) {
+	    console.log('Subscriber connected');
+
+	    //Send the first chunk immediately
+	    connection.write(
+	      '{"type":"changed","file":"targ'
+	    );
+
+	    //after a one second delay, send the other chunk
+	    let timer = setTimeout(function() {
+		    connection.write('et.txt", "timestamp":1358175758495}' + "\n");
+		    connection.end();
+		}, 1000);
+
+	    connection.on('end', function() {
+		    clearTimeout(timer);
+		    console.log('Subscriber disconnected');
+		});
+	});
+
+server.listen(5432, function() {
+	console.log('Test server listening for subscribers...');
+    });
+
+
